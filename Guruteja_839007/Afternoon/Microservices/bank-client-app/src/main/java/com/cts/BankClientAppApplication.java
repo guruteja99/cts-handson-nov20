91a03bf663f2113519fd914aace0cfd382ded53e
Guruteja <karanamguruteja@gmail.com>
2020-12-08 17:27:26 +0530

    modified config

--- Guruteja_839007/Afternoon/Microservices/bank-client-app/src/main/java/com/cts/BankClientAppApplication.java
+++ Guruteja_839007/Afternoon/Microservices/bank-client-app/src/main/java/com/cts/BankClientAppApplication.java
@@ -1,7 +1,12 @@
 package com.cts;
 
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
--- Guruteja_839007/Afternoon/Microservices/bank-client-app/src/main/java/com/cts/controller/BankController.java
+++ Guruteja_839007/Afternoon/Microservices/bank-client-app/src/main/java/com/cts/controller/BankController.java
@@ -1,10 +1,26 @@
 package com.cts.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.dao.CustomerRepository;
 
 @RestController
 @RequestMapping("/bank")
 public class BankController {
+
+	@Value("{spring.datasource.url: this is url}")
+	private String msg;
+	
+	@Autowired
+	private CustomerRepository dao;
+	
+	@GetMapping
+	public String getMessage() {
+		return msg;
+	}
 	
 }
