c63d87db0a4bcf52522b0da40959576f266ba983
Guruteja <karanamguruteja@gmail.com>
2020-12-09 10:04:10 +0530

    exercise config with server discovery

--- Guruteja_839007/Afternoon/Microservices/bank-client-app/src/main/java/com/cts/BankClientAppApplication.java
+++ Guruteja_839007/Afternoon/Microservices/bank-client-app/src/main/java/com/cts/BankClientAppApplication.java
@@ -6,10 +6,12 @@ import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BankClientAppApplication {
 
 	public static void main(String[] args) {
